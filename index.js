8da3efc04aa5662cdf3df0d8ca0fed2d7eb4f1b9
Dhiren Punjabi <dpunjabi530@gmail.com>
2020-05-25 19:55:25 +0530

    test

--- index.js
+++ index.js
@@ -49,8 +49,9 @@ app.use(order);
 app.use(orderAdm);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('website_client/build'));
   app.use('/', express.static(path.join(__dirname, 'public')));
+  app.use(express.static('website_client/build'));
+
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'website_client', 'build', 'index.html'));
   });
